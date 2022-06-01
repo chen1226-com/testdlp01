78b36b25796411b53c1ffab68f79ff12432649b8
月笼烟沙 <1226568372@qq.com>
2022-06-01 15:38:57 +0800

    Changes

---
+++ src/test.java
@@ -0,0 +1,26 @@
+public class test {
+/**dlp测试**/
+        static float x = 205F;
+
+        static float y = 572F;
+
+        private static boolean sound = true;
+
+        public BattleAppearing() {
+        }
+
+        public static void draw(Graphics g) {
+            if (sound) {
+                sound = false;
+            }
+            if (y > 428F)
+                g.drawImage(GamePanel.heroImage, (int) x, (int) y, null);
+            else if (y <= 428F) {
+                GamePanel.AppearingFlag = false;
+                x = 205F;
+                y = 572F;
+                sound = true;
+            }
+            y -= 2.0F;
+        }
+}
