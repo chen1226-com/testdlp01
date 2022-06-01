73a33cde88d60a29139e5b67a4df59afc23c1f12
月笼烟沙 <1226568372@qq.com>
2022-06-01 16:17:41 +0800

    Changes

--- src/test.java
+++ src/test.java
@@ -1,26 +1,27 @@
 public class test {
-/**dlp测试项目123**/
-        static float x = 205F;
 
-        static float y = 572F;
-/** 代码检测**/
-        private static boolean sound = true;
+    static float x = 205F;
 
-        public BattleAppearing() {
+    static float y = 572F;
+
+    private static boolean sound = true;
+
+    public BattleAppearing() {
+    }
+
+    public static void draw(Graphics g) {
+        if (sound) {
+            sound = false;
         }
-
-        public static void draw(Graphics g) {
-            if (sound) {
-                sound = false;
-            }
-            if (y > 428F)
-                g.drawImage(GamePanel.heroImage, (int) x, (int) y, null);
-            else if (y <= 428F) {
-                GamePanel.AppearingFlag = false;
-                x = 205F;
-                y = 572F;
-                sound = true;
-            }
-            y -= 2.0F;
+        if (y > 428F)
+            g.drawImage(GamePanel.heroImage, (int) x, (int) y, null);
+        else if (y <= 428F) {
+            GamePanel.AppearingFlag = false;
+            x = 205F;
+            y = 572F;
+            sound = true;
         }
+        y -= 2.0F;
+    }
+
 }
